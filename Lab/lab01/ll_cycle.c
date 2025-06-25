79d4b25cef3d9508168f1db2c408349e88cc747a
MeteorSky <meteorskymail@gmail.com>
2025-06-25 23:27:43 +0800

    lab01

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node* hare, *tortoise;
+    hare = tortoise = head;
+    while (hare && hare->next) {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise) {
+            return 1;    
+        }
+    }
     return 0;
 }
