c0c5f1d170226c73c96cb735ed0994ed7e5df715
MeteorSky <meteorskymail@gmail.com>
2025-07-01 00:26:25 +0800

    lab02 aside from memcheck

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -4,7 +4,15 @@
 #include <string.h>
 #include "lfsr.h"
 
+#define get_bit(x, n) ((x >> n) & 1u)
+
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t _0 = get_bit(*reg, 0);
+    uint16_t _2 = get_bit(*reg, 2);
+    uint16_t _3 = get_bit(*reg, 3);
+    uint16_t _5 = get_bit(*reg, 5);
+    uint16_t n = _0 ^ _2 ^ _3 ^ _5;
+    *reg = (n << 15) + (*reg >> 1);
 }
 
