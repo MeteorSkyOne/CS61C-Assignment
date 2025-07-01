73f693ae648f62ad827377048060ccf8d4fea439
MeteroSky <meteorskymail@gmail.com>
2025-07-01 23:47:17 +0800

    proj1 partA

--- Project/proj1/imageloader.c
+++ Project/proj1/imageloader.c
@@ -14,28 +14,91 @@
 **
 **************************************************************************/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <inttypes.h>
-#include <string.h>
 #include "imageloader.h"
 
-//Opens a .ppm P3 image file, and constructs an Image object. 
-//You may find the function fscanf useful.
-//Make sure that you close the file with fclose before returning.
-Image *readData(char *filename) 
-{
-	//YOUR CODE HERE
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Opens a .ppm P3 image file, and constructs an Image object.
+// You may find the function fscanf useful.
+// Make sure that you close the file with fclose before returning.
+Image *readData(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "open file err");
+        return NULL;
+    }
+    Image *img = malloc(sizeof(Image));
+    if (img == NULL) {
+        fprintf(stderr, "malloc err");
+        fclose(fp);
+        return NULL;
+    }
+    char buf[20];
+    fscanf(fp, "%s", buf);
+    if (strcmp("P3", buf) != 0) {
+        fprintf(stderr, "Wrong file format");
+        free(img);
+        fclose(fp);
+        return NULL;
+    }
+    fscanf(fp, "%u %u", &img->cols, &img->rows);
+    uint8_t range;
+    fscanf(fp, "%hhu", &range);
+    img->image = malloc(img->rows * sizeof(Color *));
+    if (img->image == NULL) {
+        fprintf(stderr, "malloc err for rows");
+        free(img);
+        fclose(fp);
+        return NULL;
+    }
+
+    for (int i = 0; i < img->rows; i++) {
+        img->image[i] = malloc(img->cols * sizeof(Color));
+        if (img->image[i] == NULL) {
+            fprintf(stderr, "malloc err for row %d", i);
+            for (int k = 0; k < i; k++) {
+                free(img->image[k]);
+            }
+            free(img->image);
+            free(img);
+            fclose(fp);
+            return NULL;
+        }
+        for (int j = 0; j < img->cols; j++) {
+            fscanf(fp, "%hhu %hhu %hhu", &img->image[i][j].R,
+                   &img->image[i][j].G, &img->image[i][j].B);
+        }
+    }
+    fclose(fp);
+    return img;
 }
 
-//Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
-void writeData(Image *image)
-{
-	//YOUR CODE HERE
+// Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the
+// image's data.
+void writeData(Image *image) {
+    printf("P3\n%u %u\n255\n", image->cols, image->rows);
+    for (int i = 0; i < image->rows; i++) {
+        for (int j = 0; j < image->cols; j++) {
+            printf("%3hhu %3hhu %3hhu", image->image[i][j].R,
+                   image->image[i][j].G, image->image[i][j].B);
+            if (j != (image->cols - 1)) {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
 }
 
-//Frees an image
-void freeImage(Image *image)
-{
-	//YOUR CODE HERE
+// Frees an image
+void freeImage(Image *image) {
+    if (image == NULL) return;
+
+    for (int i = 0; i < image->rows; i++) {
+        free(image->image[i]);
+    }
+    free(image->image);
+    free(image);
 }
--- Project/proj1/steganography.c
+++ Project/proj1/steganography.c
@@ -13,37 +13,103 @@
 **
 **************************************************************************/
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <inttypes.h>
+
 #include "imageloader.h"
 
-//Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
-Color *evaluateOnePixel(Image *image, int row, int col)
-{
-	//YOUR CODE HERE
+// Determines what color the cell at the given row/col should be. This should
+// not affect Image, and should allocate space for a new Color.
+Color *evaluateOnePixel(Image *image, int row, int col) {
+    Color *color = malloc(sizeof(Color));
+    if (color == NULL) {
+        return NULL;
+    }
+    uint8_t b = image->image[row][col].B;
+    if ((b & 0x01) == 0) {
+        color->R = color->G = color->B = 0;
+    } else {
+        color->R = color->G = color->B = 255;
+    }
+    return color;
 }
 
-//Given an image, creates a new image extracting the LSB of the B channel.
-Image *steganography(Image *image)
-{
-	//YOUR CODE HERE
+// Given an image, creates a new image extracting the LSB of the B channel.
+Image *steganography(Image *image) {
+    if (image == NULL) {
+        return NULL;
+    }
+    Image *new_image = malloc(sizeof(Image));
+    if (new_image == NULL) {
+        return NULL;
+    }
+    new_image->rows = image->rows;
+    new_image->cols = image->cols;
+    new_image->image = malloc(new_image->rows * sizeof(Color *));
+    if (new_image->image == NULL) {
+        free(new_image);
+        return NULL;
+    }
+    for (int i = 0; i < image->rows; i++) {
+        new_image->image[i] = malloc(new_image->cols * sizeof(Color));
+        if (new_image->image[i] == NULL) {
+            for (int k = 0; k < i; k++) {
+                free(new_image->image[k]);
+            }
+            free(new_image->image);
+            free(new_image);
+            return NULL;
+        }
+        for (int j = 0; j < image->cols; j++) {
+            Color *data = evaluateOnePixel(image, i, j);
+            if (data == NULL) {
+                for (int k = 0; k < i; k++) {
+                    free(new_image->image[k]);
+                }
+                free(new_image->image);
+                free(new_image);
+                return NULL;
+            }
+            new_image->image[i][j] = *data;
+            free(data);
+        }
+    }
+    return new_image;
 }
 
 /*
-Loads a file of ppm P3 format from a file, and prints to stdout (e.g. with printf) a new image, 
-where each pixel is black if the LSB of the B channel is 0, 
+Loads a file of ppm P3 format from a file, and prints to stdout (e.g. with
+printf) a new image, where each pixel is black if the LSB of the B channel is 0,
 and white if the LSB of the B channel is 1.
 
 argc stores the number of arguments.
 argv stores a list of arguments. Here is the expected input:
 argv[0] will store the name of the program (this happens automatically).
-argv[1] should contain a filename, containing a file of ppm P3 format (not necessarily with .ppm file extension).
-If the input is not correct, a malloc fails, or any other error occurs, you should exit with code -1.
-Otherwise, you should return from main with code 0.
-Make sure to free all memory before returning!
+argv[1] should contain a filename, containing a file of ppm P3 format (not
+necessarily with .ppm file extension). If the input is not correct, a malloc
+fails, or any other error occurs, you should exit with code -1. Otherwise, you
+should return from main with code 0. Make sure to free all memory before
+returning!
 */
-int main(int argc, char **argv)
-{
-	//YOUR CODE HERE
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "missing args");
+        return -1;
+    }
+
+    char *filename = argv[1];
+    Image *image = readData(filename);
+    if (image == NULL) {
+        return -1;
+    }
+    Image *new = steganography(image);
+    if (new == NULL) {
+        freeImage(image);
+        return -1;
+    }
+    writeData(new);
+    freeImage(image);
+    freeImage(new);
+    return 0;
 }
